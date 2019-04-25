c312c26ecef4514a3476f965ca6bd3435aa03459
Art Mieussens <artmieu@gmail.com>
2019-04-25 16:37:41 +0100

    Add and pass tests for basic layout sections

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,5 +5,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'home/index'
     assert_select 'title', SITE_TITLE
+    assert_select 'header'
+    assert_select 'section.user-menu'
+    assert_select 'main'
+    assert_select 'main article', 2
   end
 end
