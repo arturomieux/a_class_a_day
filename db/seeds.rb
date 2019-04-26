0069abc8ff058efca4ae442fdbc104bea83bf395
Art Mieussens <artmieu@gmail.com>
2019-04-26 15:54:43 +0100

    Add a couple of basic posts to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(title: 'First post',
+            content: 'Lorem Ipsum')
+Post.create(title: 'Second post',
+            content: 'Lorem Ipsum')
