d94d32d43d55cf6e20b59be126edfd4e72426776
Art Mieussens <artmieu@gmail.com>
2019-04-27 10:09:28 +0100

    Add test for Post description method

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Post.description gives first 200 characters of content if short_description doesn't exist" do
+    post = create(:post_without_short_description)
+    assert_equal post.description, post.content[0...500]
+  end
 end
