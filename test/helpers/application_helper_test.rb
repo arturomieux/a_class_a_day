c30ffda6f5b04ffbc4e641cc6bbb40ae5312565b
Art Mieussens <artmieu@gmail.com>
2019-05-16 16:11:07 +0100

    Refactor helper test

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,18 +2,26 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   setup do
-    travel_to Time.zone.parse('2019-05-16 07:00')
+    a_thursday = Time.zone.parse('2019-05-16 07:00')
+    travel_to a_thursday
   end
 
   test 'time_ago_in_nicer_words' do
-    assert_equal time_ago_in_nicer_words(Time.now), 'today'
-    assert_equal time_ago_in_nicer_words(Time.now.beginning_of_day), 'today'
-    assert_equal time_ago_in_nicer_words(Time.now.days_ago(1)), 'yesterday'
-    assert_equal time_ago_in_nicer_words(Time.now.days_ago(1).beginning_of_day), 'yesterday'
-    assert_equal time_ago_in_nicer_words(Time.now.days_ago(3).beginning_of_day), 'this week'
-    assert_equal time_ago_in_nicer_words(Time.now.beginning_of_week), 'this week'
-    assert_equal time_ago_in_nicer_words(Time.now.days_ago(4).beginning_of_day), 'last week'
-    assert_equal time_ago_in_nicer_words(Time.now.days_ago(7).beginning_of_week), 'last week'
+    now =  Time.now
+    assert_equal time_ago_in_nicer_words(now), 'today'
+    assert_equal time_ago_in_nicer_words(now.beginning_of_day), 'today'
+
+    yesterday = now.days_ago(1)
+    assert_equal time_ago_in_nicer_words(yesterday), 'yesterday'
+    assert_equal time_ago_in_nicer_words(yesterday.beginning_of_day), 'yesterday'
+
+    monday = now.days_ago(3).beginning_of_day
+    assert_equal time_ago_in_nicer_words(monday), 'this week'
+    assert_equal time_ago_in_nicer_words(now.beginning_of_week), 'this week'
+    
+    last_sunday = now.days_ago(4)
+    assert_equal time_ago_in_nicer_words(last_sunday.beginning_of_day), 'last week'
+    assert_equal time_ago_in_nicer_words(last_sunday.beginning_of_week), 'last week'
 
   end
 
