40f35c97cb1020557cdabc4f88c41284511ad279
Art Mieussens <artmieu@gmail.com>
2019-05-16 07:15:52 +0100

    Work in time_ago_in_nicer_words helper and its test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,23 @@
 module ApplicationHelper
 
   def time_ago_in_nicer_words(time)
-    from_time = time.to_time if ime.respond_to?(:to_time)
-    to_time = Time.now
-    from_time, to_time = to_time, from_time if from_time > to_time
 
-    distance_in_years = ((to_time - from_time)/31536000.0).round
-    distance_in_months = ((to_time - from_time)/2592000.0).round
-    distance_in_weeks = ((to_time - from_time)/604800.0).round
-    distance_in_days = ((to_time - from_time)/86400.0).round
-    distance_in_hours = ((to_time - from_time)/3600.0).round
-    distance_in_minutes = ((to_time - from_time)/60.0).round
-    distance_in_seconds = (to_time - from_time).round
-
-    case distance_in_minutes
-    when 0..55, "#{distance_in_minutes} minutes ago"
-    when 56..65, "1 hour ago"
-    when 66..95, "1 and a half hours ago"
+    if time.today?
+      'today'
+    elsif time >= Time.now.yesterday.beginning_of_day
+      'yesterday'
+    elsif time >= Time.now.beginning_of_week
+      'this week'
+    elsif time >= Time.now.days_ago(7).beginning_of_week
+      'last week'
+    elsif time >= Time.now.days_ago.beginning_of_week
+      "#{weeks} weeks ago"
+    elsif time >= Time.now.days_ago.beginning_of_week
+      "#{months} months ago"
+    elsif time >= Time.now.days_ago.beginning_of_week
+      "#{years} years ago"
     end
+
   end
   
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   setup do
+    travel_to Time.zone.parse('2019-05-16 07:00')
   end
 
-  test 'test_test' do
-    assert true
+  test 'time_ago_in_nicer_words' do
+    assert_equal time_ago_in_nicer_words(Time.now), 'today'
+    assert_equal time_ago_in_nicer_words(Time.now.beginning_of_day), 'today'
+    assert_equal time_ago_in_nicer_words(Time.now.days_ago(1)), 'yesterday'
+    assert_equal time_ago_in_nicer_words(Time.now.days_ago(1).beginning_of_day), 'yesterday'
+    assert_equal time_ago_in_nicer_words(Time.now.days_ago(3).beginning_of_day), 'this week'
+    assert_equal time_ago_in_nicer_words(Time.now.beginning_of_week), 'this week'
+    assert_equal time_ago_in_nicer_words(Time.now.days_ago(4).beginning_of_day), 'last week'
+    assert_equal time_ago_in_nicer_words(Time.now.days_ago(7).beginning_of_week), 'last week'
+
   end
 
 end
